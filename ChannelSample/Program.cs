0b251643e1aeb603796d0661e1756507e2e4b226
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-22 14:23:47 +0300

    ChannelSample project

---
+++ ChannelSample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelSample
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static async Task SingleProducerSingleConsumer()
+        {
+            var channel = Channel.CreateUnbounded<string>();
+            // In this example, the consumer keeps up with the producer
+
+            var producer1 = new Producer(channel.Writer, 1, 2000);
+        }
+    }
+}
