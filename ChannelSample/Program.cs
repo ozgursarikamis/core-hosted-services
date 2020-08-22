ac30ea6dbb527194a6765733f8d713fb80b4a6b0
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-22 14:31:55 +0300

    MultiProducerSingleConsumer

--- ChannelSample/Program.cs
+++ ChannelSample/Program.cs
@@ -25,5 +25,32 @@ namespace ChannelSample
             await producerTask1.ContinueWith(_ => channel.Writer.Complete());
             await consumerTask1;
         }
+
+        public static async Task MultiProducerSingleConsumer()
+        {
+            var channel = Channel.CreateUnbounded<string>();
+
+            // In this example, a single consumer easily keeps up with two producers
+
+            var producer1 = new Producer(channel.Writer, 1, 2000);
+            var producer2 = new Producer(channel.Writer, 2, 2000);
+            var consumer1 = new Consumer(channel.Reader, 1, 250);
+
+            Task consumerTask1 = consumer1.ConsumeData(); // begin consuming
+
+            Task producerTask1 = producer1.BeginProducing();
+
+            await Task.Delay(500); // stagger the producers
+
+            Task producerTask2 = producer2.BeginProducing();
+
+            await Task.WhenAll(producerTask1, producerTask2)
+                .ContinueWith(_ => channel.Writer.Complete());
+
+            // Writer.Complete() Marks the channel as being complete
+            // no more items will be written to it
+
+            await consumerTask1;
+        }
     }
 }
