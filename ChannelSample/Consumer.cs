38adbc40f45b11b476df4b6bddc2ad1a25a7298e
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-22 14:24:01 +0300

    Producer and Consumer classes

---
+++ ChannelSample/Consumer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelSample
+{
+    internal class Consumer
+    {
+        private readonly ChannelReader<string> _reader;
+        private readonly int _identifier;
+        private readonly int _delay;
+
+        public Consumer(ChannelReader<string> reader, int identifier, int delay)
+        {
+            _reader = reader;
+            _identifier = identifier;
+            _delay = delay;
+        }
+
+        public async Task ConsumeData()
+        {
+            Console.WriteLine($"CONSUMER ({_identifier}): Starting");
+
+            while (await _reader.WaitToReadAsync())
+            {
+                if (_reader.TryRead(out var timeString))
+                {
+                    await Task.Delay(_delay); // simulate processing time
+
+                    Console.WriteLine($"CONSUMER ({_identifier}): Consuming {timeString}");
+                }
+            }
+
+            Console.WriteLine($"CONSUMER ({_identifier}): Completed");
+        }
+    }
+}
---
+++ ChannelSample/Producer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace ChannelSample
+{
+    internal class Producer
+    {
+        private readonly ChannelWriter<string> _writer;
+        private readonly int _identifier;
+        private readonly int _delay;
+
+        public Producer(ChannelWriter<string> writer, int identifier, int delay)
+        {
+            _writer = writer;
+            _identifier = identifier;
+            _delay = delay;
+        }
+
+        public async Task BeginProducing()
+        {
+            Console.WriteLine($"PRODUCER ({_identifier}): Starting");
+
+            for (var i = 0; i < 10; i++)
+            {
+                await Task.Delay(_delay); // simulate producer building/fetching some data
+
+                var msg = $"P{_identifier} - {DateTime.UtcNow:G}";
+
+                Console.WriteLine($"PRODUCER ({_identifier}): Creating {msg}");
+
+                await _writer.WriteAsync(msg);
+            }
+
+            Console.WriteLine($"PRODUCER ({_identifier}): Completed");
+        }
+    }
+}
